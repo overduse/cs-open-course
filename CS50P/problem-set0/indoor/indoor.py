7e74f3a4fe41ea56a5f7079c3c211f10ce0125a6
Haru <carpe.tu@icloud.com>
2024-12-31 19:15:44 +0800

    feat: add CS50P problem set 0 - indoor.py

---
+++ CS50P/problem-set0/indoor/indoor.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python
+
+"""
+The script reads user input,
+converts the input to lowercase, and prints the result.
+"""
+
+
+if __name__ == "__main__":
+    user_input = input().lower()
+    print(user_input )
