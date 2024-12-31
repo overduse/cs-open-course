3ebc0ef46826f8120df6580f8dcbe033a37fb8e5
Haru <carpe.tu@icloud.com>
2025-01-01 02:01:06 +0800

    feat: add CS50P problem set 2 - camel.py

---
+++ CS50P/problem-set2/camel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+File: camel.py
+Description:
+    transform camel case to snake case.
+    camelCase -> camel_case
+
+"""
+
+
+def main():
+    usr_input = input("camelCase: ").strip()
+    for c in usr_input:
+        if(c.isupper()):
+            print('_', end='')
+            print(c.lower(), end='')
+        else:
+            print(c, end='')
+    print('\n')
+
+
+if __name__ == '__main__':
+    main()
