c75ca9c9fd07091b76e217818f9faa9d7836cb93
Haru <carpe.tu@icloud.com>
2025-01-03 04:44:57 +0800

    feat: add CS50P problem set 4 - game.py

---
+++ CS50P/problem-set4/game/game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import random
+
+def generate_num():
+    while True:
+        try:
+            lv = int(input("Level: "))
+            # print(lv)
+            if lv < 1:
+                raise ValueError
+        except ValueError:
+            pass
+        else:
+            break
+    return random.randint(1, lv)
+
+def guess(choose_num):
+    while True:
+        # geuss_num = int()
+        try:
+            guess_num = int(input("Guess: "))
+            if guess_num < 0:
+                raise ValueError
+            if guess_num > choose_num:
+                print("Too large!")
+            elif guess_num < choose_num:
+                print("Too small!")
+            else:
+                print("Just right!")
+                break
+        except ValueError:
+            pass
+
+def main():
+
+    choose_num = generate_num()
+    guess(choose_num)
+
+
+if __name__ == '__main__':
+    main()
